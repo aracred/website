df4aca3876a771513b7e5d9a0c28517746508961
burrrata <42473928+burrrata@users.noreply.github.com>
2020-04-11 18:47:33 -0400

    now the edit link is fixed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,7 +88,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
-            'https://github.com/aracred/website/edit/master/docs/',
+            'https://github.com/aracred/website/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
