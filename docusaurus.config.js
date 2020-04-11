cda86c72f131bd0ba804c57d20be41d8f017cf3d
burrrata <42473928+burrrata@users.noreply.github.com>
2020-04-11 18:45:07 -0400

    fixed edit link thing

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,7 +88,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
-            'https://github.com/aracred/website/edit/master/website/',
+            'https://github.com/aracred/website/edit/master/docs/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     Overviews: ['aracred', 'aragon','sourcecred'], 
-    Tokeneconomics: ['designGoals'],
+    Tokeneconomics: ['designGoals', 'tokenEconomyOverview'],
     Deployment: ['deploymentOverview'],
     Placeholders: ['doc1', 'doc2', 'doc3', 'mdx', 'test'],
   },
