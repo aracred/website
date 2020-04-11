0a9e4b217521c14ea40860700d70282f7ea8df8c
burrrata <42473928+burrrata@users.noreply.github.com>
2020-04-11 19:19:48 -0400

    Deploy website version based on 49af56b16491699e672947ed52ce86dcb3f9d9c2

---
+++ c043a13e.033fbb57.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[29],{128:function(a){a.exports=JSON.parse('{"allTagsPath":"/website/blog/tags","slug":"burrrata","name":"burrrata","count":2,"permalink":"/website/blog/tags/burrrata"}')}}]);
