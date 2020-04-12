d49ea3b27586437f262b74132224b280597e67bd
burrrata <42473928+burrrata@users.noreply.github.com>
2020-04-11 22:01:10 -0400

    before adding live code plugin

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     Overviews: ['aracred', 'aragon','sourcecred'], 
-    Tokeneconomics: ['designGoals', 'tokenEconomyOverview', 'tokenMintingAndSupply'],
+    Tokeneconomics: ['designGoals', 'tokenEconomyOverview', 'tokenMintingAndSupply', 'aracredModeling'],
     Deployment: ['deploymentOverview'],
     Gratitude: ['specialThanks'],
     Placeholders: ['doc1', 'doc2', 'doc3', 'mdx', 'test'],
