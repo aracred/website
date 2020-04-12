62452c343ffd843e938714bc2e87d5214d7dd315
burrrata <42473928+burrrata@users.noreply.github.com>
2020-04-11 20:18:38 -0400

    added a gratitude section

--- sidebars.js
+++ sidebars.js
@@ -3,6 +3,7 @@ module.exports = {
     Overviews: ['aracred', 'aragon','sourcecred'], 
     Tokeneconomics: ['designGoals', 'tokenEconomyOverview', 'tokenMintingAndSupply'],
     Deployment: ['deploymentOverview'],
+    Gratitude: ['specialThanks'],
     Placeholders: ['doc1', 'doc2', 'doc3', 'mdx', 'test'],
   },
 };
