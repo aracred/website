3beec5b6192940d40f8b45070519a72f670e1e80
burrrata <42473928+burrrata@users.noreply.github.com>
2020-04-24 00:47:58 -0400

    Deploy website version based on 8ea1da276230f826bd09645c1761bcffe1901505

---
+++ c043a13e.90169d4d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[36],{137:function(a){a.exports=JSON.parse('{"allTagsPath":"/website/blog/tags","slug":"burrrata","name":"burrrata","count":2,"permalink":"/website/blog/tags/burrrata"}')}}]);
